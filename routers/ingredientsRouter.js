d9cff5d53558135331e742170726e1f3f5f30afa
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-04-30 21:22:40 -0500

    finished

--- routers/ingredientsRouter.js
+++ routers/ingredientsRouter.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const ingredientsModel = require("./recipes-model");
 const db = require("../data/config");
+
+const router = express.Router({
+    mergeParams: true,
+});
